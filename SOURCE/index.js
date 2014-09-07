4befc64b75944ea0462e81e92d54a60a5d14b813
serapath <dev@serapath.de>
2014-09-07 05:04:29 +0200

    RELEASE Version 0.0.6

--- SOURCE/index.js
+++ SOURCE/index.js
@@ -2,6 +2,9 @@ module.exports = MODULE_holon;
 
 var Plates = require('plates');
 
+MODULE_holon.NAME    = require('../package.json').name.toUpperCase();
+MODULE_holon.VERSION = 'v' + require('../package.json').version;
+
 function MODULE_holon () {
   return {
     Plates: Plates,
